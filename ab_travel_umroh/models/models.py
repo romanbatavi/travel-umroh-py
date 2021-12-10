76416bad55fb34b518e3998a1699d7416ef627b3
romanbatavi <romanbatavi98@gmail.com>
2021-12-10 15:51:24 +0700

    belum di fix

--- ab_travel_umroh/models/models.py
+++ ab_travel_umroh/models/models.py
@@ -14,12 +14,23 @@ class UmrohTravel(models.Model):
             
 class PaketPerjalanan(models.Model):
     _name = 'paket.perjalanan'
-    _description = 'Travel Packageeeeee'
+    _description = 'Travel Package'
     
     tanggal_berangkat = fields.Date(string="Tanggal Berangkat")
     tanggal_kembali = fields.Date(string="Tanggal Kembali")
-    product_id = fields.Many2one('product.product', string="Sale", tracking=True)
-    bom_id = fields.Many2one('product.product', string="Package", tracking=True)
+    product_id = fields.Many2one('product.product', string="Sale")
+    bom_id = fields.Many2one('product.product', string="Package")
     quota = fields.Char(string="Quota")
     remaining_quota = fields.Char(string="Remaining Quota")
     quota_progress = fields.Char(string="Quota Progress")
+    
+#     hpp_line = field_name = fields.One2many('Bayar.paket.perjalanan', 'inverse_field_name', string='HPP Lines')
+# class BayarPaketPerjalanan(models.Model):
+#     _name = 'Bayar.paket.perjalanan'
+#     _description = 'Cost Travel Package'
+    
+#     barang = fields.Char()
+#     quantity = fields.Integer()
+#     unit = fields.Integer()
+#     unit_price = fields.Integer()
+#     subtotal = fields.Integer()
