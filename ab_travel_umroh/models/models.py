08081b31604071db38f228800d7432d0855c36a9
romanbatavi <romanbatavi98@gmail.com>
2021-12-14 15:52:08 +0700

    fix variable

--- ab_travel_umroh/models/models.py
+++ ab_travel_umroh/models/models.py
@@ -12,7 +12,7 @@ class PaketPerjalanan(models.Model):
     remaining_quota = fields.Char(string="Remaining Quota")
     quota_progress = fields.Char(string="Quota Progress")
     
-    hotel_line = fields.One2many('hotel.line', 'hotel_id', string='Hotel Lines')
+    hotels_line = fields.One2many('hotel.line', 'hotel_id', string='Hotel Lines')
     
     airlines_line = fields.One2many('airline.lines', 'airline_id', string='Airline Lines')
     
@@ -20,9 +20,9 @@ class HotelLines(models.Model):
     _name = 'hotel.line'
     _description = 'Hotel Lines'
     
-    partner_id = fields.Many2one('res.partner', string='Nama Hotel')
+    hotels_id = fields.Many2one('res.partner', string='Nama Hotel', domain=[('hotels', '=', True)])
     hotel_id = fields.Many2one('paket.perjalanan', string='Hotel Line')
-    nama_kota = fields.Char(string='Nama Kota', related='partner_id.city', tracking=True,)
+    nama_kota = fields.Char(string='Nama Kota', related='hotels_id.city', tracking=True,)
     tanggal_masuk = fields.Date(string='Tanggal Masuk')
     tanggal_keluar = fields.Date(string='Tanggal Keluar')
     
@@ -30,7 +30,7 @@ class AirlineLines(models.Model):
     _name = 'airline.lines'
     _description = 'Airline Lines'
     
-    airlines_id = fields.Many2one('res.partner', string='Nama Pesawat')
+    airlines_id = fields.Many2one('res.partner', string='Nama Pesawat', domain=[('airlines', '=', True)])
     airline_id = fields.Many2one('paket.perjalanan', String='Airlines Line')
     tanggal_berangkat = fields.Date(string='Tanggal Keberangkatan')
     kota_asal = fields.Char(string='Kota Asal')
